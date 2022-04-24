568707f03f7003d6af90e6a5805cf60c422c0ae3
Sylvia <sylvia@donchev.is>
2022-04-24 12:19:08 +0200

    function crossOut

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ $(function () {
   } else {
     $("#list").append(li); // selecting id="list" from the HTML file
   }
+  // Crossing out an item from the list of items:
+  function crossOut() {
+    li.classList.toggle("strike"); // toggle() attaches two or more functions to toggle between for the click event
+    //for the selected elements. When clicking on an element, the first specified function fires, when clicking again,
+    //the second function fires, and so on.
+  }
 });
