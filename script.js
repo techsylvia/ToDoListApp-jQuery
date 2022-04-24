b9aca0bf3178cffc468a11c6ab5d13fa9314bfa8
Sylvia <sylvia@donchev.is>
2022-04-24 12:46:38 +0200

    function named newItem & toggleClass fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //1.
 
-$(function () {
+$(function newItem() {
   let li = $("<li></li>"); // creating li for HTML
   let inputValue = $("#input").val(); // selecting the id="input" in HTML file
   $("#li").append(inputValue);
@@ -12,7 +12,7 @@ $(function () {
   }
   //2.  Crossing out an item from the list of items:
   function crossOut() {
-    li.classList.toggle("strike"); // toggle() attaches two or more functions to toggle between for the click event
+    li.toggleClass("strike"); // toggle() attaches two or more functions to toggle between for the click event
     //for the selected elements. When clicking on an element, the first specified function fires, when clicking again,
     //the second function fires, and so on.
   }
