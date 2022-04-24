85e9797187674df88c3da60c9a33d55261f7d8a5
Sylvia <sylvia@donchev.is>
2022-04-24 12:03:13 +0200

    if else added for the list in HTML

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(function () {
   let li = $("<li></li>"); // creating li for HTML
-  let inputValue = $("#input").val(); // creating input in HTML file
+  let inputValue = $("#input").val(); // selecting the id="input" in HTML file
   $("#li").append(inputValue);
+
+  if (inputValue === "") {
+    alert("Type something");
+  } else {
+    $("#list").append(li); // selecting id="list" from the HTML file
+  }
 });
